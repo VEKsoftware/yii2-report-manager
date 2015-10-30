18258edceb2fac100bf1b9357143044d384cbc48
Sergey Okatov <sokatov@gmail.com>
2015-10-30 17:21:33 +0500

    Model Reports has been generated

---
+++ models/Reports.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace veksoftware\reportmanager\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "reports".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $class_name
+ * @property string $options
+ * @property string $template
+ */
+class Reports extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'reports';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'class_name', 'options', 'template'], 'required'],
+            [['options', 'template'], 'string'],
+            [['name'], 'string', 'max' => 128],
+            [['class_name'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Название отчета',
+            'class_name' => 'Класс модели таблицы, по которой строится отчет',
+            'options' => 'Возможные дополнительные опции',
+            'template' => 'Шаблон для отчета',
+        ];
+    }
+}
