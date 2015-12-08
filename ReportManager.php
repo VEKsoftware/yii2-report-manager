adb1ab8efacb618f66db04cb1931eabd38c65f63
Sergey Okatov <sokatov@gmail.com>
2015-12-08 14:49:01 +0500

    Documentaion has been added

--- ReportManager.php
+++ ReportManager.php
@@ -4,12 +4,23 @@ namespace reportmanager;
 
 use reportmanager\models\Reports;
 
+/**
+ *
+ * The main class for module ReportManager
+ *
+ * @property $controllerNamespace The namespace for the controller in the module ReportManager
+ * @property $reporModelClass     The class which inherits reportmanager\models\Reports class
+ *
+ */
 class ReportManager extends \yii\base\Module
 {
     public $controllerNamespace = 'reportmanager\controllers';
     public $_report_classes;
     public $reportModelClass;
 
+    /**
+     * @inherit
+     */
     public function init()
     {
         parent::init();
@@ -18,16 +29,28 @@ class ReportManager extends \yii\base\Module
         Reports::$module = $this;
     }
 
+    /**
+     * Setter for classes list
+     *
+     * @param array $rc the settings from config
+     */
     public function setReportClasses($rc)
     {
         $this->_report_classes = $rc;
     }
 
+    /**
+     * Getter for classes list
+     * @return array current settings from config
+     */
     public function getReportClasses()
     {
         return $this->_report_classes;
     }
 
+    /**
+     * Initialization of the i18n translation module
+     */
     public function registerTranslations()
     {
         \Yii::$app->i18n->translations['reportmanager'] = [
